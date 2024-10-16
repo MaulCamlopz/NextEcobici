7d8a5f985f72bd8d52ebeebfbcb529429896b0d7
MaulCamlopz <b22braul@gmail.com>
2024-10-16 17:43:29 -0600

    login is added

---
+++ Next Ecobici/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  Next Ecobici
+//
+//  Created by Maul Camlopz on 16/10/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text("Login")
+    }
+}
+
+#Preview {
+    LoginView()
+}
